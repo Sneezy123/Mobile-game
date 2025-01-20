7ec5e0b6b36e0f85d46822a1c3b32ea72cd5bc93
Nils <nilshendrik13@gmail.com>
2025-01-20 21:54:24 +0100

    add tic tac toe collection

---
+++ main/game objects/tic_tac_toe_cross.go
@@ -0,0 +1,20 @@
+components {
+  id: "player_object"
+  component: "/main/scripts/games/tic tac toe/player_object.script"
+}
+embedded_components {
+  id: "sprite"
+  type: "sprite"
+  data: "default_animation: \"Cross\"\n"
+  "material: \"/builtins/materials/sprite.material\"\n"
+  "size {\n"
+  "  x: 160.0\n"
+  "  y: 160.0\n"
+  "}\n"
+  "size_mode: SIZE_MODE_MANUAL\n"
+  "textures {\n"
+  "  sampler: \"texture_sampler\"\n"
+  "  texture: \"/main/atlases/tic_tac_toe.atlas\"\n"
+  "}\n"
+  ""
+}
